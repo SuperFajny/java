25b585c8214d465667fc2a2e2f63173c01dcd30b
agata_000 <agata_000@Agata>
2017-06-21 01:20:03 +0200

    Dodany view

--- Connect four/src/start/Start.java
+++ Connect four/src/start/Start.java
@@ -1,13 +1,14 @@
 package start;
 
 import model.ConnectFive;
+import model.Modelmain;
+import widok.StartGUI;
 
 public class Start {
 
 	public static void main(String[] args) {
-		ConnectFive game = new ConnectFive();
-		game.fillZero();
-		
+		Modelmain m1 = new Modelmain(4);
+		StartGUI ap = new StartGUI();
 		
 
 	}
