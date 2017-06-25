67c826a49a35adbfc9cf5d6876229241ecfd3b2d
agata_000 <agata_000@Agata>
2017-06-25 20:00:31 +0200

    Menu

--- Connect four/src/Menu/Menu.java
+++ Connect four/src/Menu/Menu.java
@@ -1,6 +1,10 @@
 package Menu;
 
-import java.awt.EventQueue;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import start.StartGame;
 
@@ -10,12 +14,48 @@ import start.StartGame;
 //klikniecie na ConnectFive -> callGame(1)
 // okno zamyka siê po wyborze
 
-public class Menu {
+public class Menu  extends JFrame {
+	
+	public JButton b1;
+	public JButton b2;
+	JFrame ramka;
+	
 	
 	public Menu(){
-		callGame(0); // to by³o tylko do testow
+		//Menu menu = new Menu();
+		go();
+	}
+	public void go(){
+		ramka = new JFrame();
+		ramka.setLayout(new FlowLayout());
+		b1 = new JButton("ConnectFour");
+		b2 = new JButton("ConnectFive");
+		b1.addActionListener(new b1Listener());
+		b2.addActionListener(new b2Listener());
+		ramka.getContentPane().add(b1);
+		ramka.add(b2);
+		ramka.setPreferredSize(new Dimension(400, 100));
+		ramka.setMinimumSize(new Dimension(400,100));
+		ramka.setLocationRelativeTo(null);
+		ramka.setVisible(true);
+	
 	}
 	
+	class b1Listener implements ActionListener{
+		public void actionPerformed(ActionEvent e) { 
+			ramka.dispose();
+		    callGame(0);
+		    
+	 }
+		
+	}
+	class b2Listener implements ActionListener{
+		public void actionPerformed(ActionEvent e) { 
+			ramka.dispose();
+		    callGame(1);
+	}
+	}
+	     
 	public void callGame(int zasady){
 		EventQueue.invokeLater(new Runnable(){
 
